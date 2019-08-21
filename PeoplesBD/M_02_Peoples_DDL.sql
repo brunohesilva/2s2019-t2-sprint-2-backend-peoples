48afe40e4c1cc175b62f66aa494105f8cf904bb1
Bruno Henrique Silva <38940293827@info.local>
2019-08-21 08:47:24 -0300

    Domain

--- PeoplesBD/M_02_Peoples_DDL.sql
+++
@@ -1,8 +0,0 @@
-USE M_Peoples;
-
-INSERT INTO Funcionarios (Nome, Sobrenome, DataNascimento)
-VALUES ('Catarina', 'Strada', '16/11/2002');
-
-INSERT INTO Funcionarios (Nome, Sobrenome, DataNascimento)
-VALUES ('Tadeu ', 'Vitelli', '17/11/2002');
-
